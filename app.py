65bc737d1322b05b7d35277ded0b5f80585dc87b
hellohailey <65040467+hellohailey@users.noreply.github.com>
2020-06-26 22:43:06 +0900

    6/26

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ db = client.dbsparta
 def home():
      return render_template("index.html")
 
+@app.route('/api/list', methods=['GET'])
+def matzip_list():
+     matzips = list(db.matzip.find({},{'_id':False}))
+	return jsonify({'result': 'success','matzip_list': matzips})
+
 if __name__ == '__main__':
     app.run("0.0.0.0", port=5000, debug=True)
