e3783a32000d5bb2588aa89b93cbb3085de3c94d
hellohailey <65040467+hellohailey@users.noreply.github.com>
2020-06-27 17:34:32 +0900

    6/27 upload2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask (__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@127.0.0.1',27017)
 db = client.dbsparta
 
 @app.route('/')
